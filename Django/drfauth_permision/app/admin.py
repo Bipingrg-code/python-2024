6b2eb2a185fdef7a1e790e5fc5e8b98aa090470e
Bipin Gurung <bipin67678grg@gmail.com>
2024-01-30 16:31:35 +0545

    authentication and permision drf

---
+++ Django/drfauth_permision/app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Student
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id','name','roll','email','address')
+# Register your models here.
+admin.site.register(Student,StudentAdmin)
