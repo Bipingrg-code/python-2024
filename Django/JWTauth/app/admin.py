8e60c7a0e6080f1fef6d48b11da77efbaf3e4a2d
Bipin Gurung <bipin67678grg@gmail.com>
2024-02-01 13:51:39 +0545

    simple django jwt

---
+++ Django/JWTauth/app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Student
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id','name','roll','address','email']
+# Register your models here.
+admin.site.register(Student,StudentAdmin)
